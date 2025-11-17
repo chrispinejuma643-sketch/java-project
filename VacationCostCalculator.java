e04cd5753e2db83e118a7f43ebbd10599d359f02
chrispinejuma643-sketch <chrispinejuma643@gmail.com>
2025-11-17 13:04:05 +0300

    Add files via upload

---
+++ VacationCostCalculator.java
@@ -0,0 +1,12 @@
+public class VacationCostCalculator{
+       public static double calculatorTotalCost(double accommodationCost, double mealCost, double activityCost){
+           return accommodationCost + mealCost + activityCost;
+}
+           public static void main(String[]args){
+            double accommodation=900.25;
+            double meals=250.50;
+            double activities=300.00;
+            double TotalCost= calculateTotalCost(accommodation,meals,activities);
+            System.out.println("");
+           }
+        }
